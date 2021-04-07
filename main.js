e5419b09da8962e8e92a54e37068c568a6130d60
scripto <scripto@MacBook-Air-scripto.local>
2021-04-07 12:58:24 +0300

    task1 | done

---
+++ main.js
@@ -0,0 +1,43 @@
+const player1 = {
+    name: 'SUB-ZERO',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: ['knife', 'sword'],
+    attack: () => console.log(player1.name + ' - Fight')
+}
+
+const player2 = {
+    name: 'SCORPION',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['knife', 'sword'],
+    attack: () => console.log(player2.name + ' - Fight')
+}
+
+function createPlayer(className, characterName, hp) {
+    const parent = document.createElement('div'),
+        progressbar = document.createElement('div'),
+        character = document.createElement('div'),
+        life = document.createElement('div'),
+        name = document.createElement('div'),
+        img = document.createElement('img'),
+        arena = document.querySelector('.arena1');
+
+    parent.className = className
+    progressbar.className = 'progressbar'
+    life.className = 'life'
+    name.className = 'name'
+    character.className = 'character'
+    img.src = `http://reactmarathon-api.herokuapp.com/assets/${characterName}.gif`
+
+    name.textContent = characterName
+    life.textContent = hp
+
+    progressbar.prepend(life, name)
+    character.prepend(img)
+    parent.prepend(progressbar, character)
+    arena.append(parent)
+}
+
+createPlayer('player1', 'subzero', 100)
+createPlayer('player2', 'scorpion', 100)
